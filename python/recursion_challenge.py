3f5fc57f55e803c54cdb32040000d77dbd5340c1
JWShuff <16825267+JWShuff@users.noreply.github.com>
2021-06-07 16:31:03 -0400

    99 Bottles solved recursively.

--- python/recursion_challenge.py
+++ python/recursion_challenge.py
@@ -5,8 +5,28 @@ def palindrome(string):
 	pass
 
 def bottles(num, str):
-    str = ""
-    return str
+    #Base case:
+    if num == 0:
+        str += "No more bottles of beer on the wall, nor more bottles of beer. \n"
+        str += f"Go to the store and buy some more, 99 bottles of beer on the wall."
+        return str
 
+    if num == 1:
+        str += f"{num} bottle of beer on the wall, {num} bottle of beer.\n"
+        str += f"take one down and pass it around, no more bottles of beer on the wall. \n"
+        num -= 1
+        return bottles(num, str)
+
+    if num == 2:
+        str+=(f"{num} bottles of beer on the wall, {num} bottles of beer. \n")
+        str+=(f"take one down, pass it around, {num-1} bottle of beer on the wall.\n")
+        num -= 1
+        return bottles(num, str)
+
+    if num > 2:
+        str += f"{num} bottles of beer on the wall, {num} bottles of beer.\n"
+        str += f"take one down, pass it around, {num-1} bottles of beer on the wall.\n"
+        num -= 1
+        return bottles(num, str)
 def roman_num(num):
 	pass
--- python/recursion_spec.py
+++ python/recursion_spec.py
@@ -7,9 +7,19 @@ class TestRecursionChallenge(unittest.TestCase):
     99 Bottles Tests:
     """
 
-    def test_99_bottles(self):
-        self.assertEqual(type(bottles(99, 99)), str)
+    def test_0_bottles(self):
+        self.assertEqual(type(bottles(0, "")), str)
 
+    def test_1_bottles(self):
+        test_str = ""
+        test_str = (bottles(1,""))
+        # print(test_str)
+        self.assertEqual(bottles(1, ""), test_str)
+
+    def test_99_bottles(self):
+        test_str = bottles(99, "")
+        # print(test_str)
+        self.assertTrue(bottles(99, "") == test_str)
 
 if __name__ == '__main__':
     unittest.main()
