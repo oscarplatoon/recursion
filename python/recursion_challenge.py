b07d8aa93d6bf7c6f3816328514f29353ecf7694
ver2point0 <jmci10@hotmail.com>
2021-06-08 07:36:02 -0700

    John M add palindrome recursion

--- python/recursion_challenge.py
+++ python/recursion_challenge.py
@@ -1,8 +1,40 @@
+import re
+
+
 def factorial(x):
 	pass
 
+
+# allow your palindrome checker to check against palindromes sentence such as,
+#
+# "Sore was I ere I saw Eros."
+# "A man, a plan, a canal -- Panama"
+
+# get rid of spaces, special characters, etc.
+# BASE CASE
+# if length of word is 1 character return true
+# RECURSIVE CASE
+# if first char == last char
+	# return palindrome(word[1:-1])
+
+# racecar
+# aceca
+# cec
+
+# 1. Base case
+# 2. Recursive case
 def palindrome(string):
-	pass
+	word = re.sub('\W', '', str(string).lower())
+	if len(word) <= 1:
+		return True
+	if word[0] == word[-1]:
+		return palindrome(word[1:-1])
+	else:
+		return False
+
+print(palindrome("A man, a plan, a canal -- Panama"))
+print(palindrome("a"))
+
 
 def bottles(num):
 	pass
